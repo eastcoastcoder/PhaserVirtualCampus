55e3f02fcc3cf39d39a155e09ea0589fc54a5c7a
ethanx94 <coolkid602007@gmail.com>
2014-10-24 22:32:22 -0500

    Moving everything

---
+++ full.js
@@ -0,0 +1,89 @@
+
+var game = new Phaser.Game(800, 600, Phaser.AUTO, 'phaser-example', { preload: preload, create: create, update: update, render: render });
+
+function preload() {
+
+    game.load.tilemap('map', 'assets/tilemaps/maps/full.json', null, Phaser.Tilemap.TILED_JSON);
+    game.load.image('tiles', 'assets/tilemaps/tiles/pkmn.png', 32, 32);
+    game.load.spritesheet('player', 'assets/sprites/gold.png', 16, 16); // 16x16 frames
+
+}
+
+var map;
+var layer;
+var cursors;
+var player;
+
+function create() {
+
+    //  Because we're loading CSV map data we have to specify the tile size here or we can't render it
+    map = game.add.tilemap('map');
+    map.addTilesetImage('pkmn.png', 'tiles');
+    layer = map.createLayer(0);
+    layer.resizeWorld();
+
+    map.setCollisionByExclusion([22, 42, 192, 200]);
+
+    //  Un-comment this on to see the collision tiles
+    //layer.debug = true;
+
+    //  Player Sprite &Animation
+    player = game.add.sprite(2000, 910, 'player', 1);
+    player.animations.add('left', [8,9], 10, true);
+    player.animations.add('right', [1,2], 10, true);
+    player.animations.add('up', [11,12,13], 10, true);
+    player.animations.add('down', [4,5,6], 10, true);
+
+    game.physics.enable(player, Phaser.Physics.ARCADE);
+
+    //player.body.setSize(10, 14, 2, 1);
+    //padding to prevent half stepping through walls
+
+    game.camera.follow(player);
+
+    //  Allow cursors to scroll around the map
+    cursors = game.input.keyboard.createCursorKeys();
+
+    var mapname = game.add.text(16, 16, 'SIUC Virtual Campus', { font: '14px Arial', fill: '#000000' });
+    mapname.fixedToCamera = true;
+
+}
+
+function update() {
+
+    game.physics.arcade.collide(player, layer);
+
+    player.body.velocity.set(0);
+
+    if (cursors.left.isDown)
+    {
+        player.body.velocity.x = -175;
+        player.play('left');
+    }
+    else if (cursors.right.isDown)
+    {
+        player.body.velocity.x = 175;
+        player.play('right');
+    }
+    else if (cursors.up.isDown)
+    {
+        player.body.velocity.y = -175;
+        player.play('up');
+    }
+    else if (cursors.down.isDown)
+    {
+        player.body.velocity.y = 175;
+        player.play('down');
+    }
+    else
+    {
+        player.animations.stop();
+    }
+
+}
+
+function render() {
+
+     //game.debug.body(player);
+
+}
